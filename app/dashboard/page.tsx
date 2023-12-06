3f4bc278f9add0fb6fc5e9a2c2d5cecbfa53f212
QUAN <QUAN@DESKTOP-72EP28F>
2023-12-07 05:14:47 +0700

    fix error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const page = () => {
-    return (
-        <div>
-          Dashboard
-        </div>
-      )
-}
+  return <div>Dashboard</div>;
+};
 
-export default page
+export default page;
